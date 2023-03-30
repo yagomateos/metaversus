80ebb8983446be7d6c0b896129a036d1a40954d0
Yago <yagomateos@hotmail.com>
2023-03-30 18:23:46 +0200

    fix nav

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,36 +1,46 @@
-'use client';
-
 import { motion } from 'framer-motion';
-
 import styles from '../styles';
 import { navVariants } from '../utils/motion';
 
-const Navbar = () => (
-  <motion.nav
-    variants={navVariants}
-    initial="hidden"
-    whileInView="show"
-    className={`${styles.xPaddings} py-8 relative`}
-  >
-    <div className="absolute w-[50%] inset-0 gradient-01" />
-    <div
-      className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}
+const Navbar = () => {
+  const handleMenuClick = () => {
+    const exploreSection = document.getElementById('explore');
+    const topOffset = exploreSection.offsetTop + 700; // agregar un desplazamiento adicional de 100 píxeles
+    window.scrollTo({
+      top: topOffset,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <motion.nav
+      variants={navVariants}
+      initial="hidden"
+      whileInView="show"
+      className={`${styles.xPaddings} py-8 relative`}
     >
-      <img
-        src="/search.svg"
-        alt="search"
-        className="w-[24px] h-[24px] object-contain"
-      />
-      <h2 className="font-extrabold text-[24px] leading-[30.24px] text-white">
-        METAVERSUS
-      </h2>
-      <img
-        src="/menu.svg"
-        alt="menu"
-        className="w-[24px] h-[24px] object-contain"
-      />
-    </div>
-  </motion.nav>
-);
+      <div className="absolute w-[50%] inset-0 gradient-01" />
+      <div
+        className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}
+      >
+        <img
+          src="/search.svg"
+          alt="search"
+          className="w-[24px] h-[24px] object-contain"
+        />
+        <h2 className="font-extrabold text-[24px] leading-[30.24px] text-white">
+          METAVERSUS
+        </h2>
+        <img
+          src="/menu.svg"
+          alt="menu"
+          className="w-[24px] h-[24px] object-contain"
+          id="menu"
+          onClick={handleMenuClick}
+        />
+      </div>
+    </motion.nav>
+  );
+};
 
 export default Navbar;
